8336d9175fecfe54b3d5c4d3557c869112becf9a
Isaac-code-maker <playerredbrasil@gmail.com>
2025-05-02 22:56:52 -0300

    refactor: update AuthController to use DTOs for registration and login, and enhance user validation

--- unihack-backend/unihack/src/main/java/com/unihack/unihack/controllers/AuthController.java
+++ unihack-backend/unihack/src/main/java/com/unihack/unihack/controllers/AuthController.java
@@ -3,6 +3,8 @@ package com.unihack.unihack.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.unihack.unihack.dtos.RegisterDto;
+import com.unihack.unihack.dtos.LoginDto;
 import com.unihack.unihack.models.User;
 import com.unihack.unihack.repository.UserRepository;
 
@@ -11,10 +13,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import jakarta.validation.Valid;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -27,26 +32,26 @@ public class AuthController {
     private PasswordEncoder passwordEncoder; // Injetar o codificador de senha
 
     @PostMapping("/register")
-    public ResponseEntity<Map<String, String>> register(@RequestBody Map<String, String> user) {
-        if (userRepository.existsByEmail(user.get("email"))) {
+    public ResponseEntity<Map<String, String>> register(@Valid @RequestBody RegisterDto registerDto) {
+        if (userRepository.existsByEmail(registerDto.getEmail())) {
             return ResponseEntity.status(HttpStatus.CONFLICT)
                     .body(Map.of("message", "User already exists"));
         } else {
-            String encodedPassword = passwordEncoder.encode(user.get("password")); // Codificar a senha
-            User newUser = new User(user.get("name"), user.get("email"), encodedPassword);
+            String encodedPassword = passwordEncoder.encode(registerDto.getPassword()); // Codificar a senha
+            User newUser = new User(registerDto.getName(), registerDto.getEmail(), encodedPassword);
             userRepository.save(newUser);
             return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("message", "User registered successfully"));
         }
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Map<String, String>> login(@RequestBody Map<String, String> user) {
-        if (userRepository.existsByEmail(user.get("email"))) {
+    public ResponseEntity<Map<String, String>> login(@Valid @RequestBody LoginDto loginDto) {
+        User user = userRepository.findByEmail(loginDto.getEmail());
+        if (user != null && passwordEncoder.matches(loginDto.getPassword(), user.getPassword())) {
             return ResponseEntity.status(HttpStatus.OK).body(Map.of("message", "User logged in successfully"));
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                     .body(Map.of("message", "Invalid credentials"));
         }
     }
-
 }
